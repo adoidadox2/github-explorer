02d46c7412551cc70838b730307c63edc44f22b7
adoidadox2 <augusto.geek@gmail.com>
2021-02-08 23:27:46 -0300

    add Repository page style

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/65858062?s=200&v=4"
+            alt="perf"
+          />
+          <div>
+            <strong>softmakers/softmakers</strong>
+            <p>description do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>12</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>45</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="aaaa">
+          <div>
+            <strong>aaaa</strong>
+            <p>bbbbb</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
